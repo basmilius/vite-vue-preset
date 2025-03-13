19fb8e5c418f19de2faae821239a7e831998cf83
Bas Milius <basmilius@hotmail.com>
2025-03-13 15:35:11 +0100

    feat(css-modules): configurable class names. chore: bump version.

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,108 @@
-import { createHash } from 'node:crypto';
 import { resolve } from 'node:path';
+import { camelCase, kebabCase } from 'change-case';
 import { patchCssModules } from 'vite-css-modules';
 import type { Plugin } from 'vite';
+import { searchForWorkspaceRoot } from 'vite';
 import className from 'css-class-generator';
 import libAssets from '@laynezh/vite-plugin-lib-assets';
 import libDts from 'vite-plugin-dts';
 
-const presets = (isLibrary: boolean): Plugin => ({
-    name: '@basmilius/vite-vue-preset',
-
-    config: () => ({
-        build: {
-            assetsInlineLimit: 0,
-            cssMinify: 'esbuild',
-            minify: 'esbuild',
-            rollupOptions: {
-                output: {
-                    assetFileNames: isLibrary ? undefined : '[hash].[ext]',
-                    chunkFileNames: isLibrary ? undefined : '[hash].js',
-                    entryFileNames: isLibrary ? undefined : '[hash].js',
-                    compact: !isLibrary,
-                    minifyInternalExports: true
-                }
-            }
-        },
-        css: {
-            preprocessorMaxWorkers: true,
-            preprocessorOptions: {
-                scss: {
-                    api: 'modern-compiler'
-                }
-            },
-            modules: {
-                generateScopedName(name: string): string {
-                    if (name.startsWith('i__const_')) {
-                        name = name.substring(9);
-                        name = name.substring(0, name.length - 2);
-                    }
-
-                    const hash = createHash('sha1')
-                        .update(name)
-                        .digest('hex')
-                        .substring(0, 5);
-
-                    return className(parseInt(hash, 16));
-                }
-            }
-        },
-        json: {
-            stringify: true
-        },
-        resolve: {
-            alias: {
-                '@': resolve(process.cwd(), './src')
-            },
-            extensions: [
-                '.js',
-                '.ts',
-                '.json',
-                '.vue'
-            ]
-        }
-    })
-});
+const VISITED_CLASSES: string[] = [];
 
 type Options = {
+    readonly cssModules?: {
+        readonly classNames?: 'mangled' | 'camel' | 'kebab';
+        readonly generateScopedName?: (name: string, filename: string, css: string) => string;
+    };
     readonly isLibrary?: boolean;
 };
 
-export default ({isLibrary}: Options = {}) => [
+const presets = (options: Options): Plugin => {
+    function generateScopedName(name: string): string {
+        if (options.cssModules?.classNames === 'camel') {
+            return camelCase(name);
+        }
+
+        if (options.cssModules?.classNames === 'kebab') {
+            return kebabCase(name);
+        }
+
+        if (name.startsWith('i__const_')) {
+            name = name.substring(9);
+            name = name.substring(0, name.length - 2);
+        }
+
+        if (VISITED_CLASSES.includes(name)) {
+            return className(VISITED_CLASSES.indexOf(name));
+        }
+
+        return className(VISITED_CLASSES.push(name) - 1);
+    }
+
+    return {
+        name: '@basmilius/vite-vue-preset',
+
+        config: () => ({
+            build: {
+                assetsInlineLimit: 0,
+                cssMinify: 'esbuild',
+                minify: 'esbuild',
+                rollupOptions: {
+                    output: {
+                        assetFileNames: options.isLibrary ? undefined : '[hash].[ext]',
+                        chunkFileNames: options.isLibrary ? undefined : '[hash].js',
+                        entryFileNames: options.isLibrary ? undefined : '[hash].js',
+                        compact: !options.isLibrary,
+                        minifyInternalExports: true
+                    }
+                }
+            },
+            css: {
+                preprocessorMaxWorkers: true,
+                preprocessorOptions: {
+                    scss: {
+                        api: 'modern-compiler'
+                    }
+                },
+                modules: {
+                    generateScopedName: options.cssModules?.generateScopedName ?? generateScopedName,
+                }
+            },
+            json: {
+                stringify: true
+            },
+            resolve: {
+                alias: {
+                    '@': resolve(process.cwd(), './src')
+                },
+                extensions: [
+                    '.js',
+                    '.ts',
+                    '.json',
+                    '.vue'
+                ]
+            },
+            server: {
+                fs: {
+                    allow: [
+                        searchForWorkspaceRoot(process.cwd())
+                    ]
+                }
+            }
+        })
+    };
+};
+
+export default (options: Options = {}) => [
     patchCssModules(),
-    presets(isLibrary),
-    isLibrary && libAssets({
+    presets(options),
+
+    options.isLibrary && libAssets({
         limit: 0,
         name: '[contenthash:8].[ext]'
     }),
-    isLibrary && libDts({
+
+    options.isLibrary && libDts({
         cleanVueFileName: false
     })
 ] satisfies Plugin[];
