741111114c9393bf4acb34c15e527c81161c8acc
Bas Milius <basmilius@hotmail.com>
2025-03-19 08:45:54 +0100

    fix: remove ts alias. chore: bump version.

--- src/libraries.ts
+++ src/libraries.ts
@@ -1,4 +1,4 @@
-import { composeLibrary } from '@/index';
+import composeLibrary from './composeLibrary';
 
 export const flux = composeLibrary('@basmilius/flux', '$flux');
 export const fluxDashboard = composeLibrary('@basmilius/flux-dashboard', '$fluxDashboard');
